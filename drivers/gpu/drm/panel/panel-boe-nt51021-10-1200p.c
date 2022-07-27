ca02e111b5998146d4f96f65080f43ee87faaec7
Roland <guro84@gmail.com>
2022-07-27 16:08:39 +0200

    panel: MIPI_DSI_MODE_NO_EOT_PACKET

--- drivers/gpu/drm/panel/panel-boe-nt51021-10-1200p.c
+++ drivers/gpu/drm/panel/panel-boe-nt51021-10-1200p.c
@@ -259,7 +259,7 @@ static int boe_nt51021_10_1200p_probe(struct mipi_dsi_device *dsi)
 	dsi->lanes = 4;
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST |
-			  MIPI_DSI_MODE_VIDEO_HSE | MIPI_DSI_MODE_EOT_PACKET |
+			  MIPI_DSI_MODE_VIDEO_HSE | MIPI_DSI_MODE_NO_EOT_PACKET |
 			  MIPI_DSI_MODE_LPM;
 
 	drm_panel_init(&ctx->panel, dev, &boe_nt51021_10_1200p_panel_funcs,
